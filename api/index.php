cbfe416d4b4e13fff97d4e4633674794ea3c3d09
og5c5yy <og5c5yinyong@jichen.live>
2023-02-24 11:29:00 +0800

    同步api/index.php文件

--- api/index.php
+++ api/index.php
@@ -3,12 +3,16 @@ include($_SERVER['DOCUMENT_ROOT'] . "/config.php");
 include($_SERVER['DOCUMENT_ROOT'] . "/include/function.php");
 include($_SERVER['DOCUMENT_ROOT'] . "/include/user.php");
 include($_SERVER['DOCUMENT_ROOT'] . "/include/account.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/include/task.php");
-
+include($_SERVER['DOCUMENT_ROOT'] . "/include/sharepage.php");
+header('Content-type:text/json');
 global $Sys_config;
-$conn = @mysqli_connect($Sys_config["db_host"], $Sys_config["db_user"], $Sys_config["db_password"], $Sys_config["db_database"]);  //数据库连接
-if (!$conn) {
-    die("数据库连接失败：" . mysqli_connect_error());
+try {
+    $conn = new PDO("mysql:host={$Sys_config["db_host"]};dbname={$Sys_config["db_database"]};", $Sys_config["db_user"], $Sys_config["db_password"]);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // 禁用prepared statements的模拟效果
+    $conn->exec("set names utf8"); //设置编码
+} catch (PDOException $e) {
+    die("数据库连接失败，错误信息：" . $e->getMessage());
 }
 global $conn;
 if (!isset($_GET['key'])) {
@@ -29,9 +33,10 @@ if (!isset($_GET['key'])) {
 switch ($_GET["action"]) {
     case "get_task_list":
     {
-        $result = $conn->query("SELECT id FROM task;");
+        $result = $conn->prepare("SELECT id FROM account;");
+        $result->execute();
         $task_list = [];
-        while ($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch()) {
             $task_list[] = $row['id'];
         }
         $data = array(
@@ -49,14 +54,13 @@ switch ($_GET["action"]) {
                 'message' => 'id不能为空'
             );
         } else {
-            $task = new task($_GET['id']);
-            if ($task->id == -1) {
+            $account = new account($_GET['id']);
+            if ($account->id == -1) {
                 $data = array(
                     'status' => 'fail',
                     'message' => '任务不存在'
                 );
             } else {
-                $account = new account($task->account_id);
                 $question1 = array_keys($account->question)[0];
                 $question2 = array_keys($account->question)[1];
                 $question3 = array_keys($account->question)[2];
@@ -67,6 +71,7 @@ switch ($_GET["action"]) {
                     'status' => 'success',
                     'message' => '获取成功',
                     'username' => $account->username,
+                    'password' => $account->password,
                     'dob' => $account->dob,
                     'q1' => array_keys($account->question)[0],
                     'q2' => array_keys($account->question)[1],
@@ -74,35 +79,67 @@ switch ($_GET["action"]) {
                     'a1' => $account->question[$question1],
                     'a2' => $account->question[$question2],
                     'a3' => $account->question[$question3],
-                    'check_interval' => $task->check_interval,
-                    'tgbot_token' => $task->tgbot_token,
-                    'tgbot_chatid' => $task->tgbot_chatid,
+                    'check_interval' => $account->check_interval,
+                    'tgbot_token' => $Sys_config['telegram_bot_token'],
+                    'tgbot_chatid' => $Sys_config["telegram_bot_chatid"],
                     'API_key' => $Sys_config['apikey'],
                     'API_url' => $Sys_config['apiurl'],
-                    'step_sleep' => $Sys_config['backend_step_sleep'],
-                    'webdriver' => $Sys_config['webdriver_url']
+                    'webdriver' => $Sys_config['webdriver_url'],
+                    'proxy' => $Sys_config['webdriver_proxy']
 
                 );
+                if ($account->enable_check_password_correct) {
+                    $data['check_password_correct'] = true;
+                }
+                if ($account->enable_delete_devices) {
+                    $data['delete_devices'] = true;
+                }
                 break;
             }
         }
         break;
     }
-    case "get_password":{
-        if (!isset($_GET['username'])){
+    case "update_message":
+    {
+        if (!isset($_GET['username'])) {
             $data = array(
                 'status' => 'fail',
                 'message' => 'username不能为空'
             );
-        }else{
-            $result = $conn->query("SELECT password FROM account WHERE username = '".$_GET['username']."';");
-            if ($result->num_rows == 0) {
+        } else {
+            $account = new account(get_account_id($_GET['username']));
+            if ($account->id == -1) {
                 $data = array(
                     'status' => 'fail',
                     'message' => '账号不存在'
                 );
-            }else{
-                $row = $result->fetch_assoc();
+            } else {
+                $account->update_message($_GET['message']);
+                $data = array(
+                    'status' => 'success',
+                    'message' => '更新成功'
+                );
+            }
+        }
+        break;
+    }
+    case "get_password":
+    {
+        if (!isset($_GET['username'])) {
+            $data = array(
+                'status' => 'fail',
+                'message' => 'username不能为空'
+            );
+        } else {
+            $result = $conn->prepare("SELECT password FROM account WHERE username = :username;");
+            $result->execute([':username' => $_GET['username']]);
+            if ($result->rowCount() == 0) {
+                $data = array(
+                    'status' => 'fail',
+                    'message' => '账号不存在'
+                );
+            } else {
+                $row = $result->fetch();
                 $data = array(
                     'status' => 'success',
                     'message' => '获取成功',
@@ -127,7 +164,7 @@ switch ($_GET["action"]) {
                     'message' => '账号不存在'
                 );
             } else {
-                $account->update_password($_GET['password']==""?$account->password:$_GET['password']);
+                $account->update_password($_GET['password']);
                 $data = array(
                     'status' => 'success',
                     'message' => '更新成功'
@@ -142,6 +179,30 @@ switch ($_GET["action"]) {
             'status' => 'success',
             'message' => 'API正常'
         );
+        break;
+    }
+    case "random_sharepage_password":
+    {
+        if (!isset($_GET['id'])) {
+            $data = array(
+                'status' => 'fail',
+                'message' => '页面ID不能为空'
+            );
+        }
+        $sharepage = new sharepage($_GET['id']);
+        if ($sharepage->id == -1) {
+            $data = array(
+                'status' => 'fail',
+                'message' => '页面不存在'
+            );
+        } else {
+            $sharepage->randomPassword();
+            $data = array(
+                'status' => 'success',
+                'password' => $sharepage->password,
+                'message' => '更新成功'
+            );
+        }
         break;
     }
     default:
